56e2c36571441edc237d81233f5792584a93f80c
Darryl James <datael@datael.co.uk>
2022-12-02 21:09:06 +0900

    Define main structs and converters

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,1 +1,51 @@
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl From<String> for Shape {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "A" | "X" => Self::Rock,
+            "B" | "Y" => Self::Paper,
+            "C" | "Z" => Self::Scissors,
+            _ => panic!("Invalid input"),
+        }
+    }
+}
+
+struct Score(i32);
+
+impl From<Shape> for Score {
+    fn from(value: Shape) -> Self {
+        match value {
+            Shape::Rock => Score(1),
+            Shape::Paper => Score(2),
+            Shape::Scissors => Score(3),
+        }
+    }
+}
+
+struct Round {
+    their_shape: Shape,
+    our_shape: Shape,
+}
+
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl From<Outcome> for Score {
+    fn from(value: Outcome) -> Self {
+        match value {
+            Outcome::Lose => Score(0),
+            Outcome::Draw => Score(3),
+            Outcome::Win => Score(6),
+        }
+    }
+}
+
 fn main() {}
