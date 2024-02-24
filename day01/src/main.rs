19ea51322ba728b8786a1080bd519fd08768c7ff
Darryl James <datael@datael.co.uk>
2024-02-25 00:13:44 +0900

    Solve Day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,22 +1,72 @@
-const INPUT_TEST: &str = include_str!("../input_test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    // let test_result = solve_part1(INPUT_TEST, 7., 27.);
-    // println!("Test Part 1: {}", test_result);
-    // assert!(test_result == 2);
+    let part_1_result = solve_part1(INPUT);
+    println!("Part 1: {}", part_1_result);
 
-    // let part_1_result = solve_part1(INPUT, 200_000_000_000_000., 400_000_000_000_000.);
-    // println!("Part 1: {}", part_1_result);
-
-    // let test_result = solve_part2(INPUT_TEST);
-    // println!("Test Part 2: {}", test_result);
-    // assert!(test_result == 47);
-
-    // let part_2_result = solve_part2(INPUT);
-    // println!("Part 2: {}", part_2_result);
+    let part_2_result = solve_part2(INPUT);
+    println!("Part 2: {}", part_2_result);
 }
 
-fn solve_part1(input: &str) -> usize {
-    todo!()
+fn solve_part1(input: &str) -> isize {
+    let line = input.lines().next().unwrap();
+
+    let mut floor = 0;
+    for c in line.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => unreachable!(),
+        }
+    }
+    floor
+}
+
+fn solve_part2(input: &str) -> usize {
+    let line = input.lines().next().unwrap();
+
+    let mut floor = 0;
+    for (i, c) in line.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => unreachable!(),
+        }
+
+        if floor == -1 {
+            return i + 1;
+        }
+    }
+
+    unreachable!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        for (input, expected) in [
+            ("(())", 0),
+            ("()()", 0),
+            ("(((", 3),
+            ("(()(()(", 3),
+            ("))(((((", 3),
+            ("())", -1),
+            ("))(", -1),
+            (")))", -3),
+            (")())())", -3),
+            (")())())\n", -3),
+        ] {
+            assert_eq!(solve_part1(input), expected);
+        }
+    }
+
+    #[test]
+    fn test_part2() {
+        for (input, expected) in [(")", 1), ("()())", 5)] {
+            assert_eq!(solve_part2(input), expected);
+        }
+    }
 }
