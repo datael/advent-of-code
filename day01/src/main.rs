e55c3e955ebfdcb6c95f7e96988b982f931a89cd
Darryl James <datael@datael.co.uk>
2024-02-24 23:56:36 +0900

    Initial Commit

---
+++ day01/src/main.rs
@@ -0,0 +1,22 @@
+const INPUT_TEST: &str = include_str!("../input_test.txt");
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    // let test_result = solve_part1(INPUT_TEST, 7., 27.);
+    // println!("Test Part 1: {}", test_result);
+    // assert!(test_result == 2);
+
+    // let part_1_result = solve_part1(INPUT, 200_000_000_000_000., 400_000_000_000_000.);
+    // println!("Part 1: {}", part_1_result);
+
+    // let test_result = solve_part2(INPUT_TEST);
+    // println!("Test Part 2: {}", test_result);
+    // assert!(test_result == 47);
+
+    // let part_2_result = solve_part2(INPUT);
+    // println!("Part 2: {}", part_2_result);
+}
+
+fn solve_part1(input: &str) -> usize {
+    todo!()
+}
