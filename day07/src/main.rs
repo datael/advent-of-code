841f30d897fa16b3c9b3474b974850d7130a841b
Darryl James <datael@datael.co.uk>
2024-12-07 18:48:15 +0900

    Solve Day 07 part 2 with recursion, too
    It's depressing how much faster this version is...

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
 
     let part_2_result = solve_part2(INPUT);
     println!("Part 2: {}", part_2_result);
+
+    let part_2_result = solve_part2_alt(INPUT);
+    println!("Part 2: {}", part_2_result);
 }
 
 fn solve_part1(input: &str) -> usize {
@@ -56,6 +59,55 @@ fn solve_part2(input: &str) -> usize {
     input.lines().flat_map(solve_for_base::<3>).sum()
 }
 
+fn solve_part2_alt(input: &str) -> usize {
+    input.lines().flat_map(solve_alt).sum()
+}
+
+fn solve_alt(line: &str) -> Option<usize> {
+    let (target, numbers) = line.split_once(": ").unwrap();
+
+    let target = target.parse::<usize>().unwrap();
+    let numbers = numbers
+        .split(" ")
+        .flat_map(str::parse::<usize>)
+        .collect::<Vec<_>>();
+
+    if solve_alt_inner(&numbers, target, numbers[0], 1) {
+        return Some(target);
+    } else {
+        return None;
+    }
+
+    fn solve_alt_inner(
+        numbers: &Vec<usize>,
+        target: usize,
+        running_total: usize,
+        index: usize,
+    ) -> bool {
+        if index == numbers.len() {
+            return running_total == target;
+        }
+
+        return solve_alt_inner(numbers, target, running_total + numbers[index], index + 1)
+            || solve_alt_inner(numbers, target, running_total * numbers[index], index + 1)
+            || solve_alt_inner(
+                numbers,
+                target,
+                {
+                    let mut rt = running_total;
+                    let mut nn = numbers[index];
+                    while nn > 0 {
+                        rt *= 10;
+                        nn /= 10;
+                    }
+                    rt += numbers[index];
+                    rt
+                },
+                index + 1,
+            );
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -93,6 +145,7 @@ mod tests {
             11387,
         )] {
             assert_eq!(solve_part2(input), expected);
+            assert_eq!(solve_part2_alt(input), solve_part2(input));
         }
     }
 }
